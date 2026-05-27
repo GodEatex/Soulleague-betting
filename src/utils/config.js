6ab8b9280df2153cd408e1d54b0e65ba113490f2
GodEatex <nautnauti337@gmail.com>
2026-05-27 23:58:17 +0200

    IconFix

--- src/utils/config.js
+++ src/utils/config.js
@@ -5,22 +5,26 @@ const DEFAULTS = {
   warTriggerChannelId: null,
   logsChannelId: null,
   resultChannelId: null,
-  resultDetectionChannelId: null, // NEW: channel bot watches for match reports
-  forcecreateRoleId: null,        // NEW: role that can use /forcecreate
-  currencyName: 'Coins',
-  currencyEmoji: '🪙',
+  resultDetectionChannelId: null,
+  forcecreateRoleId: null,
+  currencyName: process.env.CURRENCY_NAME || 'Coins',
+  currencyEmoji: process.env.CURRENCY_EMOJI || '🪙',
 };
 
 function getConfig(guildId) {
   const all = storage.read('guild_configs');
-  return Object.assign({}, DEFAULTS, all[guildId] || {});
+  const guildData = all[guildId] || {};
+  return Object.assign({}, DEFAULTS, guildData, {
+    currencyName: process.env.CURRENCY_NAME || guildData.currencyName || DEFAULTS.currencyName,
+    currencyEmoji: process.env.CURRENCY_EMOJI || guildData.currencyEmoji || DEFAULTS.currencyEmoji,
+  });
 }
 
 async function setConfig(guildId, updates) {
   const all = storage.read('guild_configs');
-  all[guildId] = Object.assign({}, DEFAULTS, all[guildId] || {}, updates);
+  all[guildId] = Object.assign({}, all[guildId] || {}, updates);
   await storage.write('guild_configs', all);
-  return all[guildId];
+  return getConfig(guildId);
 }
 
 module.exports = { getConfig, setConfig };
